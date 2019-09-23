b588a750524f109f1f994d5453eacd7d36c27596
Simon <simon@warrigal.lu>
2019-09-23 19:45:08 +0200

    get stream infos from youtube id

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,9 +3,17 @@ import 'package:provider/provider.dart';
 import 'package:redditify/presenters/posts/posts_list.dart';
 import 'package:redditify/presenters/posts/posts_loading_button.dart';
 import 'package:redditify/states/posts_state.dart';
+import 'package:youtube_extractor/youtube_extractor.dart';
 
 class HomePage extends StatelessWidget {
+  YouTubeExtractor extractor = YouTubeExtractor();
+
   final List<String> _subreddits = const ["metalcore", "metal", "deathcore"];
+
+  void _getAudioStream() async {
+    var streamInfo = await extractor.getMediaStreamsAsync('rqMrZlLJXRA');
+    print("Audio url : ${streamInfo.audio.first.url}");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +26,12 @@ class HomePage extends StatelessWidget {
         body: ListView(
           shrinkWrap: true,
           children: <Widget>[
-            for (String subreddit in _subreddits)
-              PostsLoadingButton(
-                subreddit: subreddit,
-              ),
-            PostsList(),
+            RaisedButton(child: Text("Test extractor"), onPressed: _getAudioStream,)
+            // for (String subreddit in _subreddits)
+            //   PostsLoadingButton(
+            //     subreddit: subreddit,
+            //   ),
+            // PostsList(),
           ],
         ),
       ),

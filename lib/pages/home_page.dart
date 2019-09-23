83a12c6762a990cd92973b4cd6f54df987e2d8fc
Simon <simon@warrigal.lu>
2019-09-23 19:56:49 +0200

    test a play button from stream url

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:redditify/presenters/posts/posts_list.dart';
@@ -7,12 +8,17 @@ import 'package:youtube_extractor/youtube_extractor.dart';
 
 class HomePage extends StatelessWidget {
   YouTubeExtractor extractor = YouTubeExtractor();
+  var streamInfo;
 
   final List<String> _subreddits = const ["metalcore", "metal", "deathcore"];
 
   void _getAudioStream() async {
-    var streamInfo = await extractor.getMediaStreamsAsync('rqMrZlLJXRA');
+    streamInfo = await extractor.getMediaStreamsAsync('rqMrZlLJXRA');
     print("Audio url : ${streamInfo.audio.first.url}");
+  }
+
+  void _playFromUrl() async {
+    int result = await AudioPlayer().play(streamInfo.audio.first.url);
   }
 
   @override
@@ -26,7 +32,14 @@ class HomePage extends StatelessWidget {
         body: ListView(
           shrinkWrap: true,
           children: <Widget>[
-            RaisedButton(child: Text("Test extractor"), onPressed: _getAudioStream,)
+            RaisedButton(
+              child: Text("Test extractor"),
+              onPressed: _getAudioStream,
+            ),
+            RaisedButton(
+              child: Text("play"),
+              onPressed: _playFromUrl,
+            )
             // for (String subreddit in _subreddits)
             //   PostsLoadingButton(
             //     subreddit: subreddit,

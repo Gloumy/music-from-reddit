91d55b096d5463b50c48c09dda10e243fb4d27d2
Simon <simon@warrigal.lu>
2019-09-28 15:25:07 +0200

    move globalstate provider above materialapp to make it really global

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Redditify',
-      navigatorKey: Routes.sailor.navigatorKey, // important
-      onGenerateRoute: Routes.sailor.generator(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider<GlobalState>.value(
-            value: _globalState,
-          ),
-          ChangeNotifierProvider<SubredditsState>.value(
-            value: _globalState.subredditsState,
-          ),
-          ChangeNotifierProvider<PostsState>.value(
-            value: _globalState.postsState,
-          ),
-          ChangeNotifierProvider<PlayerState>.value(
-            value: _globalState.playerState,
-          ),
-        ],
-        child: HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<GlobalState>.value(
+          value: _globalState,
+        ),
+        ChangeNotifierProvider<SubredditsState>.value(
+          value: _globalState.subredditsState,
+        ),
+        ChangeNotifierProvider<PostsState>.value(
+          value: _globalState.postsState,
+        ),
+        ChangeNotifierProvider<PlayerState>.value(
+          value: _globalState.playerState,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Redditify',
+        navigatorKey: Routes.sailor.navigatorKey, // important
+        onGenerateRoute: Routes.sailor.generator(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
       ),
     );
   }

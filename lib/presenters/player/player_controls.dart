663bbefbe448a7606f0f08c17596553fc4b9efde
Simon <simon@warrigal.lu>
2019-09-29 21:24:16 +0200

    pause/stop/resume

--- lib/presenters/player/player_controls.dart
+++ lib/presenters/player/player_controls.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:redditify/states/player_state.dart';
@@ -16,9 +17,19 @@ class PlayerControls extends StatelessWidget {
                 child: Icon(Icons.skip_previous),
                 onTap: () => state.playPreviousSong(),
               ),
+              if (state.audioPlayerState == AudioPlayerState.PAUSED)
+                GestureDetector(
+                  child: Icon(Icons.play_arrow),
+                  onTap: () => state.resumeAudio(),
+                ),
+              if (state.audioPlayerState == AudioPlayerState.PLAYING)
+                GestureDetector(
+                  child: Icon(Icons.pause_circle_outline),
+                  onTap: () => state.pauseAudio(),
+                ),
               GestureDetector(
-                child: Icon(Icons.play_arrow),
-                onTap: () {},
+                child: Icon(Icons.stop),
+                onTap: () => state.stopAudio(),
               ),
               GestureDetector(
                 child: Icon(Icons.skip_next),
--- lib/states/player_state.dart
+++ lib/states/player_state.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:redditify/models/post.dart';
 import 'package:redditify/services/play_audio_service.dart';
@@ -7,11 +8,22 @@ class PlayerState with ChangeNotifier {
   bool _isPlaying = false;
   List<Post> _playlist = [];
   int _currentSongIndex;
+  AudioPlayerState _audioPlayerState;
 
   PlayAudioService get playAudioService => _playAudioService;
   bool get isPlaying => _isPlaying;
   List<Post> get playlist => List.from(_playlist);
   int get currentSongIndex => _currentSongIndex;
+  AudioPlayerState get audioPlayerState => _audioPlayerState;
+
+  PlayerState() {
+    _playAudioService.audioPlayer.onPlayerStateChanged
+        .listen((AudioPlayerState state) {
+      _audioPlayerState = state;
+      print(state);
+      notifyListeners();
+    });
+  }
 
   String get currentSongTitle {
     return _playlist[_currentSongIndex].title;
@@ -31,12 +43,18 @@ class PlayerState with ChangeNotifier {
     _isPlaying = true;
     notifyListeners();
     _playAudioService.audioPlayer.onPlayerCompletion.listen((event) {
-      playNextSong();
+      if (_currentSongIndex < _playlist.length) {
+        playNextSong();
+      } else {
+        stopAudio();
+      }
     });
   }
 
   Future<void> stopAudio() async {
     _playAudioService.audioPlayer.stop();
+    _currentSongIndex = 0;
+    _playlist = [];
     _isPlaying = false;
     notifyListeners();
   }
@@ -51,5 +69,13 @@ class PlayerState with ChangeNotifier {
     _currentSongIndex--;
     notifyListeners();
     _playAudioService.playAudio(_playlist[_currentSongIndex].url);
+  }
+
+  Future<void> pauseAudio() async {
+    _playAudioService.audioPlayer.pause();
+  }
+
+  Future<void> resumeAudio() async {
+    _playAudioService.audioPlayer.resume();
   }
 }

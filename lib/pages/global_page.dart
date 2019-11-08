6e19dfccd2e38179188457abb673ccbf4b383aa2
Simon <simon@warrigal.lu>
2019-11-08 16:49:43 +0100

    open close panel on tap on current song title

--- lib/pages/global_page.dart
+++ lib/pages/global_page.dart
@@ -9,30 +9,38 @@ import 'package:redditify/widgets/playlist_panel.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class GlobalPage extends StatelessWidget {
+  final PanelController _panelController = PanelController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlidingUpPanel(
-        minHeight: 75,
-        body: Container(
-          height: double.maxFinite,
-          width: double.maxFinite,
-          child: Consumer<GlobalState>(
-            builder: (context, state, _) {
-              return IndexedStack(
-                index: state.visibleIndex,
-                children: <Widget>[
-                  HomePage(),
-                  SubredditGenrePage(),
-                  SubredditPage(),
-                ],
-              );
-            },
+      body: Provider<PanelController>.value(
+        value: _panelController,
+        child: SlidingUpPanel(
+          minHeight: 75,
+          body: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            child: Consumer<GlobalState>(
+              builder: (context, state, _) {
+                return IndexedStack(
+                  index: state.visibleIndex,
+                  children: <Widget>[
+                    HomePage(),
+                    SubredditGenrePage(),
+                    SubredditPage(),
+                  ],
+                );
+              },
+            ),
+            padding: EdgeInsets.only(bottom: 75),
           ),
-          padding: EdgeInsets.only(bottom: 75),
+          panel: PlaylistPanel(),
+          collapsed: CollapsedPlayer(),
+          controller: _panelController,
+          backdropOpacity: 0.5,
+          backdropEnabled: true,
         ),
-        panel: PlaylistPanel(),
-        collapsed: CollapsedPlayer(),
       ),
     );
   }
--- lib/presenters/player/current_song_title.dart
+++ lib/presenters/player/current_song_title.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:redditify/states/player_state.dart';
 import 'package:redditify/utils/colors.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class CurrentSongTitle extends StatelessWidget {
   @override
@@ -10,12 +11,23 @@ class CurrentSongTitle extends StatelessWidget {
       builder: (context, state, _) {
         return Expanded(
           flex: 8,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              state.currentSongTitle ?? "No current song playing.",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: lightGreyColor),
+          child: GestureDetector(
+            onTap: () {
+              PanelController panelController =
+                  Provider.of<PanelController>(context, listen: false);
+              if (panelController.isPanelOpen()) {
+                panelController.close();
+              } else {
+                panelController.open();
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                state.currentSongTitle ?? "No current song playing.",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: lightGreyColor),
+              ),
             ),
           ),
         );
